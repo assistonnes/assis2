21c581e58d239aa25d356c1d957153efe660a666
assistonnes <assistonnes@gmail.com>
2026-02-01 14:43:45 +0530

    Update staff.js

--- staff.js
+++ staff.js
@@ -136,7 +136,7 @@
   const KEY_MAP = {
 
     C: {
-      a:{slot:"C",acc:""},  b:{slot:"C",acc:"#"},
+      a:{slot:"C",acc:"#"},  b:{slot:"C",acc:"#"},
       c:{slot:"D",acc:""},  d:{slot:"D",acc:"#"},
       e:{slot:"E",acc:""},
       f:{slot:"F",acc:""},  g:{slot:"F",acc:"#"},
